5b3ac477716a593a1e5a4c710bf2f16b63e2dbd1
Eko Suprapto Wibowo <swdev.bali@gmail.com>
2013-01-28 22:40:39 +0700

    initial commit

---
+++ SenzalaYogyakartaAdmin/Form1.cs
@@ -0,0 +1,31 @@
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SenzalaYogyakartaAdmin
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string connectionString = "mongodb://swdev:muhammad@linus.mongohq.com:10030/senzalayogyakarta";
+            MongoClient client = new MongoClient(connectionString);
+            MongoServer  server = client.GetServer();
+            MongoDatabase db = server.GetDatabase("senzalayogyakarta");
+            MongoCollection siswa = db.GetCollection("siswa");
+            MessageBox.Show(siswa.Count() + "");
+        }
+    }
+}
